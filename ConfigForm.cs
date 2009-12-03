d57fce01d5c30941ae898ca6d0ebe22410596c0d
Robert <xperimental@solidproject.de>
2009-12-03 20:56:11 +0100

    Documentation for ConfigForm class.

--- ConfigForm.cs
+++ ConfigForm.cs
@@ -3,8 +3,18 @@
     using System;
     using System.Windows.Forms;
 
+    /// <summary>
+    /// The ConfigForm enables the user to enter his login information for
+    /// github.
+    /// </summary>
     internal partial class ConfigForm : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the ConfigForm class.
+        /// The current configuration values are loaded from the Config
+        /// singleton instance, so the configuration has to be initialized
+        /// before displaying this window.
+        /// </summary>
         public ConfigForm()
         {
             this.InitializeComponent();
@@ -12,12 +22,21 @@
             this.LoadValues();
         }
 
+        /// <summary>
+        /// Load the values from the configuration object into the
+        /// window controls.
+        /// </summary>
         private void LoadValues()
         {
             this.textUser.Text = Config.Instance.User;
             this.textToken.Text = Config.Instance.Token;
         }
 
+        /// <summary>
+        /// This method is called when the user clicks on the Save button.
+        /// </summary>
+        /// <param name="sender">Event sender (ignored).</param>
+        /// <param name="e">Event arguments (ignored).</param>
         private void ButtonSave_Click(object sender, EventArgs e)
         {
             if (this.textUser.Text.Length < 1)
@@ -44,6 +63,10 @@
             this.Close();
         }
 
+        /// <summary>
+        /// This method saves the values from the window controls
+        /// to the configuration object.
+        /// </summary>
         private void SaveValues()
         {
             Config.Instance.User = this.textUser.Text;
