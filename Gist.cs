69226898183f1b916e4ba8af1743aa892e4f9f04
Robert <xperimental@solidproject.de>
2009-12-03 20:29:52 +0100

    Documentation for Gist class.

--- Gist.cs
+++ Gist.cs
@@ -1,23 +1,47 @@
 ﻿namespace GistBoard
 {
+    /// <summary>
+    /// This class represents a Gist which should be sent to the server.
+    /// </summary>
     internal class Gist
     {
+        /// <summary>
+        /// Contains the name of the code snippet.
+        /// </summary>
         private string fileName;
+
+        /// <summary>
+        /// Contains the gist data.
+        /// </summary>
         private string contents;
+
+        /// <summary>
+        /// True, if the gist should be marked private.
+        /// </summary>
         private bool privateGist;
 
+        /// <summary>
+        /// Gets or sets the gist name.
+        /// </summary>
         public string Filename
         {
             get { return this.fileName; }
             set { this.fileName = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the gist contents.
+        /// </summary>
         public string Contents
         {
             get { return this.contents; }
             set { this.contents = value; }
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the gist
+        /// should be marked private.
+        /// </summary>
         public bool Private
         {
             get { return this.privateGist; }
