cce4bea5b5370b8fa8f2f36168c96899764b3847
Robert <xperimental@solidproject.de>
2009-12-03 20:44:09 +0100

    Documentation for PostForm class.

--- PostForm.cs
+++ PostForm.cs
@@ -3,8 +3,18 @@
     using System;
     using System.Windows.Forms;
 
+    /// <summary>
+    /// The PostForm window is shown to the user, so that he can
+    /// enter the information required to create a new Gist.
+    /// The information is sent to the server via the GistServer class
+    /// if the user clicks on the Post button.
+    /// </summary>
     public partial class PostForm : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the PostForm class.
+        /// The Gist contents are read from the clipboard.
+        /// </summary>
         public PostForm()
         {
             this.InitializeComponent();
@@ -12,6 +22,13 @@
             this.textContent.Text = Clipboard.GetText();
         }
 
+        /// <summary>
+        /// This method is called when the user clicks on the
+        /// Post button. If all information is present, then the Gist
+        /// is sent to the server.
+        /// </summary>
+        /// <param name="sender">Event sender (ignored).</param>
+        /// <param name="e">Event arguments (ignored).</param>
         private void ButtonPost_Click(object sender, EventArgs e)
         {
             Gist gist = new Gist();
